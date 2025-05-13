4fcb3688e5c14bb4e57b3cad14e7c824e2ec8f99
satoshicano <satoshicano@gmail.com>
2025-05-13 16:32:12 +0900

    ロガーを追加して Hello World の取得をログに記録する

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name, { timestamp: true });
+
   getHello(): string {
+    this.logger.log('Getting Hello World!');
     return 'Hello World!';
   }
 }
